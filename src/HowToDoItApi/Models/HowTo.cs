c50827446f4d6c600c72ba0e5b8a42e3da2e304b
Jakub Czartek <kuba@linux.pl>
2021-10-31 15:07:38 +0100

    Added HowTo model to Api Project.

---
+++ src/HowToDoItApi/Models/HowTo.cs
@@ -0,0 +1,11 @@
+namespace HowToDoItApi.Models
+{
+    public class HowTo
+    {
+        public int HowToId { get; set; }
+        public string HowTo {get; set;}
+        public string Platform { get; set; }
+        public string Solution { get; set; }
+        public string NotesToSolution {get; set;}
+    }
+}
