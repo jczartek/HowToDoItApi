3c27bef4fc12e7b435299468c94dc2016c94bcf9
Jakub Czartek <kuba@linux.pl>
2021-10-31 15:04:05 +0100

    Fix name project

--- src/HowToDoItApi/Program.cs
+++ src/HowToDoItApi/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace HowToDoIt.Api
+namespace HowToDoItApi
 {
     public class Program
     {
